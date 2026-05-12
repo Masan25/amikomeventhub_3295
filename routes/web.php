051cff3f325368d4ae28b2d372209d599c4fbcb8
Masan25 <sandyhare8@gmail.com>
2026-05-12 08:34:03 +0700

    Kosongkan repository

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/mahasiswa', function () {
+    return 'I Komang Sandy Hare Nandhana - 24.12.3295';
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
